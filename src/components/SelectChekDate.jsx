71cb1614051affaee08a6dacba7b660c746751bb
khamidullox <hamidullo22022@gmail.com>
2024-11-17 23:07:58 +0500

    add

--- src/components/SelectChekDate.jsx
+++ src/components/SelectChekDate.jsx
@@ -11,8 +11,8 @@ function SelectChekDate() {
       }}
       className="select select-info w-28 max-w-xs"
     >
-      <option value={0}>Август</option>
       <option value={1}>Октябрь</option>
+      <option value={0}> Август</option>
     </select>
   );
 }
